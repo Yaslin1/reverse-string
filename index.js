18b3b186d76191518d24f4abd3ef546a18c6bd27
Yaslin Carmona <yaslincarmona@Yaslins-MacBook-Air.local>
2023-06-20 23:21:01 -0400

    reverse-string

---
+++ index.js
@@ -0,0 +1,13 @@
+//reverse a string
+
+let pups = "My pups are the best!";
+
+function pupsReverse (rev) {
+    let pupString = "";
+    for(i = 0; i < 9; i++); {
+        pupString = i[0] + pups;
+    }
+    return pupString
+}
+
+console.log (pupsReverse(pups))
